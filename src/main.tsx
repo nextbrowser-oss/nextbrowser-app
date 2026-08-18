fb57d630865792ecc76b8aabfd2546ee9d388c4d
AndreiCrimezniuc <kib69dev19@gmail.com>
2026-08-18 21:47:17 +0300

    refine light theme with Sandy styling

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import "@fontsource/hanken-grotesk/400.css";
 import "@fontsource/hanken-grotesk/500.css";
 import "@fontsource/hanken-grotesk/600.css";
 import "@fontsource/hanken-grotesk/700.css";
-import "@fontsource/space-grotesk/400.css";
-import "@fontsource/space-grotesk/500.css";
-import "@fontsource/space-grotesk/600.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 import "@fontsource/jetbrains-mono/500.css";
 import "./styles.css";
 
